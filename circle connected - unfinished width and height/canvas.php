b1dc0dd423f6acbca33eacd6a122581a6806662b
freenoor <rinorsahiti70@gmail.com>
2020-10-13 22:20:12 +0200

    first commit

---
+++ circle connected - unfinished width and height/canvas.php
@@ -0,0 +1,50 @@
+<!-- 
+Template Name: Canvas
+-->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <title>Canvas r</title>
+    
+</head>
+
+<body>
+
+
+<canvas id="c"></canvas>
+
+
+
+
+
+<style type="text/css">
+
+*{
+    margin:0;
+    padding:0;
+    }
+body{
+    background:#222;
+    } 
+canvas{
+    position:absolute;
+    border:5px solid rgba(255,255,255,0.1);
+    box-shadow:inset 0 0 100px #4162a9;
+    transform:translateZ(0);
+    }
+</style> 
+
+    <script src="<?= get_template_directory_uri(); ?>/assets/canvas/canvas.js"></script>
+
+    
+
+</body>
+</html>
+
+
+
+
---
+++ xplod grid/canvas.php
@@ -0,0 +1,44 @@
+<!-- 
+Template Name: Canvas
+-->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <title>Canvas r</title>
+    
+</head>
+
+<body>
+
+
+<canvas id="c"></canvas>
+
+    <div id="container"></div>
+
+</div>
+
+
+<style type="text/css">
+
+html, body{
+    background: #111
+    }
+#container{
+  background: #111;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+    }
+
+</style> 
+
+
+<script src="<?= get_template_directory_uri(); ?>/assets/canvas/canvas.js"></script>
+
+
+</body>
+</html>
